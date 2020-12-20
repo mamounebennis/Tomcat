0897c76065bd6e387312cf21cd27eece94342277
[mamounebennis1] <[kaka1181@hotmail.com]>
2020-12-20 18:46:58 +0100

    Tomcat

---
+++ src/main/java/com/example/partie2jk/Partie2jkApplication.java
@@ -0,0 +1,13 @@
+package com.example.partie2jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Partie2jkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Partie2jkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/partie2jk/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.partie2jk;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Partie2jkApplication.class);
+    }
+
+}
